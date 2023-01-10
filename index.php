c4d18bd21e47946352da3fcff076e719c12a6154
Lander Sola <landersolaaguirre@gmail.com>
2023-01-10 12:35:39 +0100

    add

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
-
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <title>Files Explorer</title>
 </head>
 
@@ -22,6 +22,65 @@
             <button class="btn btn-outline-success" type="submit">Search</button>
         </form>
     </nav>
+    <div id="directory">
+        <?php
+        $path = '/Applications/XAMPP/xamppfiles/htdocs/Assembler/Files PHP/filesystem-explorer/uploads';
+        $files = array_values(array_diff(scandir($path), array('.', '..')));
+        echo '<ul class="list-group">';
+        foreach ($files as $file) {
+            $extension = pathinfo($file, PATHINFO_EXTENSION);
+            switch ($extension) {
+                case "txt":
+                    $icono = "notes";
+                    break;
+                case "doc":
+                case "docx":
+                    $icono = "description";
+                    break;
+                case "pdf":
+                    $icono = "picture_as_pdf";
+                    break;
+                case "csv":
+                    $icono = "border_all";
+                    break;
+                case "jpg":
+                case "png":
+                    $icono = "image";
+                    break;
+                case "ppt":
+                    $icono = "mdi mdi-file-powerpoint-box";
+                    break;
+                case "odt":
+                    $icono = "mdi mdi-file-document-box";
+                    break;
+                case "zip":
+                case "rar":
+                    $icono = "mdi mdi-file-zip-box";
+                    break;
+                case "exe":
+                    $icono = "mdi mdi-file-cog";
+                    break;
+                case "svg":
+                    $icono = "mdi mdi-file-image";
+                    break;
+                case "mp3":
+                    $icono = "mdi mdi-file-music-outline";
+                    break;
+                case "mp4":
+                    $icono = "mdi mdi-file-video-outline";
+                    break;
+                default:
+                    $icono = "mdi mdi-file-outline";
+            };
+            echo '<li class="list-group-item"
+                      <span class="material-icons">'. $icono .'</span> 
+                      <span>' . $file . '</span>
+                  </li>';
+        };
+        echo '</ul>'
+        ?>
+
+    </div>
 </body>
 
 </html>
