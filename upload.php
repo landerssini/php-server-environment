7a5934fbd996f4103ecd428d7aca96868cc10b5c
luisao8 <74673031+luisao8@users.noreply.github.com>
2023-01-10 16:17:19 +0100

    new changes

--- upload.php
+++ upload.php
@@ -1,75 +1,52 @@
 <?php
-// if (isset($_POST['submit'])) {
-//     $file = $_FILES['file'];
 
-//     $fileName = $_FILES['file']['name'];
-//     $fileTmpName = $_FILES['file']['tmp_name'];
-//     $fileSize = $_FILES['file']['size'];
-//     $fileError = $_FILES['file']['error'];
-//     $fileType = $_FILES['file']['type'];
-    
-//     $fileExt = explode('.', $fileName);
-//     $fileActualExt = strtolower(end($fileExt));
-
-//     $allowed = array('jpg', 'png', 'doc','csv', 'jpeg', 'pdf','txt', 'jpeg', 'ppt','zip', 'rar', 'svg','csv', 'mp3', 'mp4');
-
-//     if(in_array($fileActualExt, $allowed)) {
-//         if ($fileError === 0) {
-//             if ($fileSize < 5000) {
-//                 $fileNameNew = uniqid('', true).".".$fileActualExt;
-//                 $fileDestination = 'uploads/'.$fileNameNew;
-//                 move_uploaded_file($fileTmpName, $fileDestination);
-//                 header('Location: index.php')
-//             } else {
-//                 echo 'File is too big.';
-//             }
-//         } else { 
-//             echo 'This type of file is not allowed.';
-//         }
-//         } else { 
-//             echo 'There was an error with you file.';
-//     }
-
-// }
-
-//Check if the form has been submitted
-if(isset($_POST['submit'])) {
-    //Retrieve the file from the form
+if(isset($_POST['submit'])){
+    //Taking the files from input
     $file = $_FILES['file'];
+    //Getting the file name of the uploaded file
+    $fileName = $_FILES['file']['name'];
+    //Getting the Temporary file name of the uploaded file
+    $fileTempName = $_FILES['file']['tmp_name'];
+    //Getting the file size of the uploaded file
+    $fileSize = $_FILES['file']['size'];
+    //getting the no. of error in uploading the file
+    $fileError = $_FILES['file']['error'];
+    //Getting the file type of the uploaded file
+    $fileType = $_FILES['file']['type'];
 
-    //Get file properties
-    $fileName = $file['name'];
-    $fileTmpName = $file['tmp_name'];
-    $fileSize = $file['size'];
-    $fileError = $file['error'];
-    $fileType = $file['type'];
+    //Getting the file ext
+    $fileExt = explode('.',$fileName);
+    $fileActualExt = strtolower(end($fileExt));
 
-    //Extract file extension
-    $fileExt = pathinfo($fileName, PATHINFO_EXTENSION);
-
-    //Allowed extensions
+    //Array of Allowed file type
     $allowedExt = array("jpg", "jpeg", "png", "pdf", "txt", "doc", "csv", "zip", "rar", "svg", "mp3", "mp4");
 
-    //Check if extension is allowed
-    if(in_array($fileExt, $allowedExt)) {
-        //Check for errors
-        if($fileError === 0) {
-            //Check for file size
-            if($fileSize < 5000000) {
-                //Create unique file name
-                $fileNewName = uniqid("", true).".".$fileExt;
-                //Define file destination
-                $fileDestination = "/Applications/XAMPP/xamppfiles/htdocs/Assembler/Files PHP/filesystem-explorer/uploads/".$fileNewName;
-                move_uploaded_file($fileNewName, $fileDestination);
-                header("Location: index.php?Success?".$fileNewName);
-            } else {
-                echo "File size too large. Maximum allowed size is 5MB.";
+    //Checking, Is file extentation is in allowed extentation array
+    if(in_array($fileActualExt, $allowedExt)){
+        //Checking, Is there any file error
+        if($fileError == 0){
+            //Checking,The file size is bellow than the allowed file size
+            if($fileSize < 10000000){
+                //Creating a unique name for file
+                $fileNemeNew = uniqid('',true).".".$fileActualExt;
+                //File destination
+                $fileDestination = 'uploads/'.$fileNemeNew;
+                //function to move temp location to permanent location
+                move_uploaded_file($fileTempName, $fileDestination);
+                //Message after success
+                header("Location: index.php?Success");
+            }else{
+                //Message,If file size greater than allowed size
+                echo "File Size Limit beyond acceptance";
             }
-        } else {
-            echo "Error uploading the file. Error code: ".$fileError;
+        }else{
+            //Message, If there is some error
+            echo "Something Went Wrong Please try again!";
         }
-    } else {
-        echo "File type not allowed.";
+    }else{
+        //Message,If this is not a valid file type
+        echo "You can't upload this extention of file";
     }
 }
+
 ?>
